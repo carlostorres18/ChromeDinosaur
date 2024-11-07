e2ca30e49d8ad0ebc586ba31122ee6310af5de87
carlostorres18 <torreccarlos@gmail.com>
2024-11-07 12:28:48 -0600

    Updated img folder name and added Image variables.

--- src/ChromeDinosaur.java
+++ src/ChromeDinosaur.java
@@ -13,10 +13,34 @@ public class ChromeDinosaur extends JPanel {
     int boardWidth = 750;
     int boardHeight = 250;
 
+    // images will go here
+    Image dinosaurImg; // running animated image (gif)
+    Image dinosaurDead;
+    Image dinosaurJumpImg;
+    Image cactus1Img;
+    Image cactus2Img;
+    Image cactus3Img;
+    Image bigCactus1Img;
+    Image bigCactus2Img;
+    Image bigCactus3Img;
     
 
     public ChromeDinosaur(){
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         setBackground(Color.lightGray);
+
+        // Short version, this functions gets the image
+        // getClass() to get into ChromeDinosaur class, getResource() this get the img folder into the Chrome... class
+        // and it uses the url to choose the image, getImage() gets the actual image
+        dinosaurImg = new ImageIcon(getClass().getResource("./img/dino-run.gif")).getImage();
+        dinosaurDead = new ImageIcon(getClass().getResource("./img/dino-dead.pgn")).getImage();
+        dinosaurJumpImg = new ImageIcon(getClass().getResource("./img/dino-jump.png")).getImage();
+        cactus1Img = new ImageIcon(getClass().getResource("./img/cactus1.png")).getImage();
+        cactus2Img = new ImageIcon(getClass().getResource("./img/cactus2.png")).getImage();
+        cactus3Img = new ImageIcon(getClass().getResource("./img/cactus3.png")).getImage();
+        bigCactus1Img = new ImageIcon(getClass().getResource("./img/big-cactus1.png")).getImage();
+        bigCactus2Img = new ImageIcon(getClass().getResource("./img/big-cactus2.png")).getImage();
+        bigCactus3Img = new ImageIcon(getClass().getResource("./img/big-cactus3.png")).getImage();
+
     }
 }
